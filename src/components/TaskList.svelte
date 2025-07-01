b2fcdb2d5399a0e897d589f517dfa207a7cd0823
yutao <obayashi21249@cs.saisoncard.co.jp>
2025-07-01 18:25:50 +0900

    created TaskList.svelte

---
+++ src/components/TaskList.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Task from './Task.svelte';
+
+    // Sets the loading state
+    export let loading = false;
+
+    // Defines a list of tasks
+    export let tasks = [];
+
+    // Reactive declaration
+    $: noTasks = tasks.length === 0;
+    $: emptyTasks = noTasks && !loading;
+</script>
+
+{#if loading}
+    <div class="list-items">loading</div>
+{/if}
+{#if emptyTasks}
+    <div class="list-items">empty</div>
+{/if}
+{#each tasks as task}
+    <Task {task} on:onPinTask on:onArchiveTask />
+{/each}
