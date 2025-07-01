8ae05436d8d87a25c2e57039c8a9102e16d58665
yutao <obayashi21249@cs.saisoncard.co.jp>
2025-07-01 23:24:19 +0900

    updated TaskList.svelte

--- src/components/TaskList.svelte
+++ src/components/TaskList.svelte
@@ -1,5 +1,6 @@
 <script>
     import Task from './Task.svelte';
+    import LoadingRow from './LoadingRow.svelte';
 
     // Sets the loading state
     export let loading = false;
@@ -10,13 +11,29 @@
     // Reactive declaration
     $: noTasks = tasks.length === 0;
     $: emptyTasks = noTasks && !loading;
+    $: tasksInorder = [
+        ...tasks.filter((t) => t.state === 'TASK_PINNED'),
+        ...tasks.filter((t) => t.state !== 'TASK_PINNED')
+    ];
 </script>
 
 {#if loading}
-    <div class="list-items">loading</div>
+    <div class="list-items">
+        <LoadingRow />
+        <LoadingRow />
+        <LoadingRow />
+        <LoadingRow />
+        <LoadingRow />
+    </div>
 {/if}
 {#if emptyTasks}
-    <div class="list-items">empty</div>
+    <div class="list-items">
+        <div class="wrapper-message">
+            <span class="icon-check" />
+            <p class="title-message">You have no tasks</p>
+            <p class="subtitle-message">Sit back and relax</p>
+        </div>
+    </div>
 {/if}
 {#each tasks as task}
     <Task {task} on:onPinTask on:onArchiveTask />
