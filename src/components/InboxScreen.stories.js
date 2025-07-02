50e0c8aacf5d6907d09d5c30b5b29d76961a7f7d
yutao <obayashi21249@cs.saisoncard.co.jp>
2025-07-02 09:19:44 +0900

    created InboxScreen.stories.js

---
+++ src/components/InboxScreen.stories.js
@@ -0,0 +1,13 @@
+import InboxScreen from "./InboxScreen.svelte";
+
+export default {
+    component: InboxScreen,
+    title: 'InboxScreen',
+    tags: ['autodocs'],
+};
+
+export const Default = {};
+
+export const Error = {
+    args: { error: true },
+};
